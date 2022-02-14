ae6d5515565f9034d3b444ab11bcfc1d574877c6
tqk2811 <tqk2811@gmail.com>
2022-02-14 11:32:17 +0700

    using Nito.AsyncEx for single thread context

--- TqkLibrary.Queues/TaskQueues/TaskQueue.cs
+++ TqkLibrary.Queues/TaskQueues/TaskQueue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nito.AsyncEx;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,42 +9,93 @@ using System.Threading.Tasks;
 
 namespace TqkLibrary.Queues.TaskQueues
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IQueue : IDisposable
     {
+        /// <summary>
+        /// Prioritize
+        /// </summary>
         bool IsPrioritize { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         bool ReQueue { get; }
+
+        /// <summary>
+        /// ReQueue After AllQueue Completed
+        /// </summary>
         bool ReQueueAfterRunComplete { get; }
 
         /// <summary>
         /// Dont use <b>async void</b> inside<br/>
-        /// Use Task.Start(function_return_void) or async Task 
         /// </summary>
         /// <returns></returns>
         Task DoWork();
 
+        /// <summary>
+        /// 
+        /// </summary>
         void Cancel();
     }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="task"></param>
+    /// <param name="queue"></param>
 
     public delegate void QueueComplete<T>(Task task, T queue) where T : IQueue;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="isRequeue"></param>
 
     public delegate void RunComplete(bool isRequeue);
+    /// <summary>
+    /// 
+    /// </summary>
 
     public delegate void QueueNextParty();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="ae"></param>
+    /// <param name="queue"></param>
 
     public delegate void TaskException<T>(AggregateException ae, T queue) where T : IQueue;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class TaskQueue<T> where T : IQueue
     {
         private readonly List<T> _Queues = new List<T>();
         private readonly List<T> _Runnings = new List<T>();
         private readonly List<T> _ReQueues = new List<T>();
         private readonly ManualResetEvent manualResetEvent = new ManualResetEvent(false);
-
+        /// <summary>
+        /// 
+        /// </summary>
         public event RunComplete OnRunComplete;
+        /// <summary>
+        /// 
+        /// </summary>
         public event QueueComplete<T> OnQueueComplete;
+        /// <summary>
+        /// 
+        /// </summary>
         public event QueueNextParty OnQueueNextParty;
 
         private int _MaxRun = 0;
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int MaxRun
         {
             get { return _MaxRun; }
@@ -56,34 +108,64 @@ namespace TqkLibrary.Queues.TaskQueues
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public bool IsRunning { get { return RunningCount != 0 || QueueCount != 0 || ReQueues.Count != 0; } }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public List<T> Queues { get { return _Queues.ToList(); } }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public List<T> Runnings { get { return _Runnings.ToList(); } }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public List<T> ReQueues { get { return _ReQueues.ToList(); } }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int RunningCount
         {
             get { return _Runnings.Count; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int QueueCount
         {
             get { return _Queues.Count; }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int ReQueueCount
         {
             get { return _ReQueues.Count; }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public bool RunRandom { get; set; } = false;
 
         /// <summary>
         /// Ex: Add 10 items, MaxRun = 2. Then 2 next threads will run after 2 threads end
         /// </summary>
         public bool RunAsParty { get; set; } = false;
-        public TaskScheduler TaskScheduler { get; set; } = TaskScheduler.FromCurrentSynchronizationContext();
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public TaskScheduler TaskScheduler { get; set; } = TaskScheduler.Default;
 
 
         //need lock Queues first
@@ -93,8 +175,13 @@ namespace TqkLibrary.Queues.TaskQueues
             {
                 _Queues.Remove(queue);
                 lock (_Runnings) _Runnings.Add(queue);
-                Task.Factory.StartNew(queue.DoWork, CancellationToken.None, TaskCreationOptions.LongRunning, this.TaskScheduler)
-                  .Unwrap()
+                Task.Factory.StartNew(() =>
+                {
+                    AsyncContext.Run(async () => await queue.DoWork());
+                    //SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+                    //return queue.DoWork();
+                }, CancellationToken.None, TaskCreationOptions.LongRunning, this.TaskScheduler)
+                  //.Unwrap()
                   .ContinueWith(this.ContinueTaskResult, queue);
             }
         }
@@ -168,6 +255,11 @@ namespace TqkLibrary.Queues.TaskQueues
         //  //_Queues.gro
         //}
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Add(T queue)
         {
             if (null == queue) throw new ArgumentNullException(nameof(queue));
@@ -175,6 +267,11 @@ namespace TqkLibrary.Queues.TaskQueues
             RunNewQueue();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="queues"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void AddRange(IEnumerable<T> queues)
         {
             if (null == queues) throw new ArgumentNullException(nameof(queues));
@@ -182,6 +279,11 @@ namespace TqkLibrary.Queues.TaskQueues
             RunNewQueue();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Cancel(T queue)
         {
             if (null == queue) throw new ArgumentNullException(nameof(queue));
@@ -189,6 +291,11 @@ namespace TqkLibrary.Queues.TaskQueues
             lock (_Runnings) foreach (var q in _Runnings.Where(x => x.Equals(queue))) q.Cancel();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="func"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Cancel(Func<T, bool> func)
         {
             if (null == func) throw new ArgumentNullException(nameof(func));
@@ -212,6 +319,11 @@ namespace TqkLibrary.Queues.TaskQueues
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Reset(T queue)
         {
             if (null == queue) throw new ArgumentNullException(nameof(queue));
@@ -219,6 +331,9 @@ namespace TqkLibrary.Queues.TaskQueues
             Add(queue);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void ShutDown()
         {
             MaxRun = 0;
@@ -234,15 +349,25 @@ namespace TqkLibrary.Queues.TaskQueues
             lock (_ReQueues) _ReQueues.Clear();
         }
 
-        public void WaitForShutDown(int timeOut = -1)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timeOut"></param>
+        public bool WaitForShutDown(int timeOut = -1)
         {
             if (RunningCount > 0)
             {
-                manualResetEvent.WaitOne(timeOut);
+                return manualResetEvent.WaitOne(timeOut);
             }
+            return true;
         }
 
-        public Task WaitForShutDownAsync(int timeOut = -1)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timeOut"></param>
+        /// <returns></returns>
+        public Task<bool> WaitForShutDownAsync(int timeOut = -1)
           => Task.Run(() => WaitForShutDown(timeOut));
     }
 }
