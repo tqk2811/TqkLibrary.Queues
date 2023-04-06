87a4b937872470cb6245a5e4276f43bc24051db7
tqk2811 <tqk2811@gmail.com>
2023-04-06 14:42:21 +0700

    add WaitQueue

--- TqkLibrary.Queues/WaitQueues/WaitQueue.cs
+++ TqkLibrary.Queues/WaitQueues/WaitQueue.cs
@@ -1,91 +1,105 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading;
+﻿using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace TqkLibrary.Queues.WaitQueues
-//{
-//  public delegate void SetStatusFreeDelegate<T>(T sender);
+namespace TqkLibrary.Queues.WaitQueues
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IWaitHandle : IDisposable
+    {
 
-//  public interface IWait
-//  {
-//    event SetStatusFreeDelegate<IWait> Free;
-//  }
+    }
 
-//  //max 64 wait
-//  public class WaitQueue<T> where T : IWait
-//  {
-//    private readonly List<Tuple<T, AutoResetEvent>> waits = new List<Tuple<T, AutoResetEvent>>();
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WaitQueue
+    {
+        readonly AsyncLock asyncLock = new AsyncLock();
+        readonly AsyncLock asyncLockAccess = new AsyncLock();
 
-//    /// <summary>
-//    /// warning: dont add when something is waiting
-//    /// </summary>
-//    /// <param name="wait"></param>
-//    public void Add(T wait)
-//    {
-//      if (null == wait) throw new ArgumentNullException(nameof(wait));
-//      if (waits.Count >= 64) throw new Exception("Limit at 64 wait");
-//      wait.Free += Wait_Free;
-//      waits.Add(new Tuple<T, AutoResetEvent>(wait, new AutoResetEvent(false)));
-//    }
+        int _currentAccess = 0;
 
-//    private void Wait_Free(IWait sender)
-//    {
-//      waits.Where(x => x.Item1.Equals(sender)).FirstOrDefault()?.Item2.Set();
-//    }
+        int _MaxAccess = 2;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MaxAccess
+        {
+            get { return _MaxAccess; }
+            set { _MaxAccess = value; AccessChanged?.Invoke(); }
+        }
+        event Action AccessChanged;
+        /// <summary>
+        /// 
+        /// </summary>
+        public WaitQueue()
+        {
+        }
+        private void _Increment()
+        {
+            Interlocked.Increment(ref _currentAccess);
+        }
+        private void _Decrement()
+        {
+            Interlocked.Decrement(ref _currentAccess);
+            AccessChanged?.Invoke();
+        }
 
-//    /// <summary>
-//    /// warning: dont remove when something is waiting
-//    /// </summary>
-//    /// <param name="wait"></param>
-//    public void Remove(T wait)
-//    {
-//      if (null == wait) throw new ArgumentNullException(nameof(wait));
-//      wait.Free -= Wait_Free;
-//      waits.Remove(waits.Where(x => x.Item1.Equals(wait)).FirstOrDefault());
-//    }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<IWaitHandle> WaitLockAsync(CancellationToken cancellationToken = default)
+        {
+            using (var l = await asyncLock.LockAsync(cancellationToken).ConfigureAwait(false))
+            {
+                while (_currentAccess >= _MaxAccess)
+                {
+                    TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+                    using var register = cancellationToken.Register(() => tcs.TrySetCanceled());
+                    Action action = () => tcs.TrySetResult(true);
+                    try
+                    {
+                        AccessChanged += action;
+                        if (_currentAccess >= _MaxAccess)
+                        {
+                            await tcs.Task.ConfigureAwait(false);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    finally
+                    {
+                        AccessChanged -= action;
+                    }
 
-//    public T WaitAny()
-//    {
-//      return WaitAny(TimeSpan.MaxValue);
-//    }
+                }
+                return new WaitHandle(this);
+            }
+        }
 
-//    public T WaitAny(TimeSpan timeSpan)
-//    {
-//      //autoResetEvent.WaitOne(TimeSpan.Zero) -> if set return true (free)
-//      //search free
-//      var con = waits.Where(x => x.Item2.WaitOne(TimeSpan.Zero)).OrderBy(x => Guid.NewGuid()).FirstOrDefault();
-//      if (null != con)
-//      {
-//        con.Item2.Reset();//busy mode
-//        return con.Item1;
-//      }
-//      else
-//      {
-//        //wait
-//        DateTime dateTime = DateTime.Now;
-//        if (Monitor.TryEnter(waits, timeSpan))//lock, only one wait
-//        {
-//          try
-//          {
-//            TimeSpan timespanleft = DateTime.Now - dateTime;//time left
-//            AutoResetEvent[] waitHandles = waits.Select(x => x.Item2).ToArray();
-//            int freeIndex = WaitHandle.WaitAny(waitHandles, timeSpan.Subtract(timespanleft));
-//            if (freeIndex == WaitHandle.WaitTimeout) return default;//timeout
-//            else
-//            {
-//              Tuple<T, AutoResetEvent> tuple = waits.Where(x => x.Item2.Equals(waitHandles[freeIndex])).FirstOrDefault();
-//              tuple.Item2.Reset();//busy mode
-//              return tuple.Item1;
-//            }
-//          }
-//          finally
-//          {
-//            Monitor.Exit(waits);
-//          }
-//        }
-//        else return default;//timeout
-//      }
-//    }
-//  }
-//}
+        class WaitHandle : IWaitHandle
+        {
+            readonly WaitQueue waitQueue;
+            public WaitHandle(WaitQueue waitQueue)
+            {
+                this.waitQueue = waitQueue ?? throw new ArgumentNullException(nameof(waitQueue));
+                this.waitQueue._Increment();
+            }
+            public void Dispose()
+            {
+                waitQueue._Decrement();
+            }
+        }
+    }
+}
