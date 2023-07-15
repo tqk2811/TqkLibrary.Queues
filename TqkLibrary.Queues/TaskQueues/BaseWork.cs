e95450e435dcaf4381aee90bf6e3b68b1dfe7798
tqk2811 <tqk2811@gmail.com>
2023-07-15 17:43:10 +0700

    add BaseWork

---
+++ TqkLibrary.Queues/TaskQueues/BaseWork.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Queues.TaskQueues
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BaseWork : IWork
+    {
+        CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected CancellationToken CancellationToken { get { return _cancellationTokenSource.Token; } }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual bool IsPrioritize { get; set; } = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void Cancel()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void Dispose()
+        {
+            _cancellationTokenSource.Dispose();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public abstract Task DoWork();
+    }
+}
